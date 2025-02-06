eea0c5973851fa187c654681b187e5e4dcf628f4
jhimi01 <talukderjhimi@gmail.com>
2025-02-06 15:28:01 +0600

    first commit

---
+++ src/app/components/auth/LoginForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LoginForm() {
+  return (
+    <div>
+      this is a login form
+    </div>
+  )
+}
---
+++ src/app/components/auth/SignUpForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SignUpForm() {
+  return (
+    <div>
+      this is a signup page
+    </div>
+  )
+}
